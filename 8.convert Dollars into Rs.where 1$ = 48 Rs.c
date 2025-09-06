8d5eb7bfe46484e7d3a91235b3685ea19c412ca7
aryankpatel2112-cell <aryankpatel2112@gmail.com>
2025-09-06 19:53:24 +0530

    Add files via upload

---
+++ 8.convert Dollars into Rs.where 1$ = 48 Rs.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int dollers,rs;
+    printf("enter money in dollers");
+    scanf("%d",&dollers);
+    rs = dollers * 48;
+    printf("rs = %d = %d * 48\n",rs,dollers);
+    return 0;
+}
---
+++ 9.convert rupees into doller.where doller=48rupeeys.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int doller,rupeey;
+    printf("enter money in rupeey");
+    scanf("%d",&rupeey);
+    doller = rupeey/48;
+    printf("doller = %d = %d / 48\n",doller,rupeey);
+    return 0;
+}
