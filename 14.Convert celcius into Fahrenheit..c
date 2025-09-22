17e5c1e6a4b98c84a9cf34b54971f2555e3707c2
aryankpatel2112-cell <aryankpatel2112@gmail.com>
2025-09-22 10:30:33 +0530

    Add files via upload

---
+++ 14.Convert celcius into Fahrenheit..c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int C,F;
+    printf("Enter Temprture in celcius :");
+    scanf("%d",&C);
+    F = ((9*C)/5)+32;
+    printf("F = %d \n",F);
+    return 0;
+}
